c1cc11c694d59f2430935df5d896d5d4449833e3
Jeton Ramadani <jeton.ramadani@students.finki.ukim.mk>
2024-09-04 14:45:43 +0200

    Stage 1 code

---
+++ 001. Simple Chat Bot/Stage 1 - Meet your chat bot/SimpleBot.java
@@ -0,0 +1,9 @@
+public class SimpleBot {
+    public static void main(String[] args) {
+        String name = "Toni";
+        int createdYear = 2024;
+
+        System.out.println("Hello my name is " + name + ".");
+        System.out.println("I was created in " + createdYear + ".");
+    }
+}
