015d7f5242429e8010e0a1622bab073a173aa238
Jeton Ramadani <jeton.ramadani@students.finki.ukim.mk>
2024-10-31 20:20:37 +0100

    Stage 1 of Zookeeper With Java

---
+++ 004/ Zookeeper with Java/Stage 1 - Print first lines of code/ZookeeperWithJava1.java
@@ -0,0 +1,10 @@
+public class ZookeeperWithJava1 {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("I love animals!\n" +
+                "Let's check on the animals...\n" +
+                "The deer looks fine.\n" +
+                "The bat looks happy.\n" +
+                "The lion looks healthy.");
+    }
+}
--- README.md
+++ README.md
@@ -6,3 +6,5 @@
      - Project Folder [here](002/%20My%20First%20Project%20with%20Java)
 003. [Chuck Norris Cipher Encoder](https://hyperskill.org/projects/293)
         - Project Folder [here](003/%20Chuck%20Norris%20Cipher%20Encoder)
+004. [Zookeeper with Java](https://hyperskill.org/projects/229)
+        - Project Folder [here](004/%20Zookeeper%20with%20Java)
