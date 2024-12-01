21cef41d657ffef8141b7b5374db92a4b15db26b
Jeton Ramadani <69119243+jetonramadani@users.noreply.github.com>
2024-12-01 03:18:08 +0100

    Project/simple tic tac toe (#11)
    * Stage 1 of Simple Tic Tac Toe
    
    * Stage 2 of Simple Tic Tac Toe
    
    * Stage 3 of Simple Tic Tac Toe
    
    * Stage 4 of Simple Tic Tac Toe
    
    * Stage 5 of Simple Tic Tac Toe

---
+++ 006/ Simple Tic-Tac-Toe/Stage 2 - The user is the gamemaster/TicTacToeSimple2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TicTacToeSimple2 {
+    public static void printBoard(char[] plays) {
+        System.out.println("---------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(plays[i * 3 + j] + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("---------");
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // write your code here
+        System.out.print("Enter cells: ");
+        char[] plays = sc.next().toCharArray();
+        printBoard(plays);
+    }
+}
---
+++ 006/ Simple Tic-Tac-Toe/Stage 4 - First to move/TicTacToeSimple4.java
@@ -0,0 +1,144 @@
+
+import java.util.HashMap;
+import java.util.Scanner;
+class TicTackSimple4 {
+    private char[] plays;
+    private final Scanner sc;
+    private int x_;
+    private int o_;
+    private boolean xMove;
+    static final int [][] winCombos = {
+            {0, 1, 2},
+            {0, 3, 6},
+            {0, 4, 8},
+            {1, 4, 7},
+            {3, 4, 5},
+            {2, 5, 8},
+            {2, 4, 6},
+            {6, 7, 8}
+    };
+    static final HashMap<String, Integer> movesToIndex =
+            new HashMap<>() {
+                {
+                    put("1.1", 0);
+                    put("1.2", 1);
+                    put("1.3", 2);
+                    put("2.1", 3);
+                    put("2.2", 4);
+                    put("2.3", 5);
+                    put("3.1", 6);
+                    put("3.2", 7);
+                    put("3.3", 8);
+                }
+            };
+    TicTackSimple4(Scanner sc) {
+        this.sc = sc;
+        setCells();
+    }
+    private void setCells() {
+        System.out.print("Enter cells: ");
+        plays = sc.next().toCharArray();
+        for (char ch : plays) {
+            if (ch == 'X') {
+                ++x_;
+            } else if (ch == 'O') {
+                ++o_;
+            }
+        }
+//        xMove = x_ <= o_;
+        xMove = true;
+    }
+    public void move() {
+        while (true) {
+            System.out.print("Enter the coordinates: ");
+            String search = sc.nextInt() + "." + sc.nextInt();
+            try {
+                Double.parseDouble(search);
+                if (movesToIndex.containsKey(search)) {
+                    int ind = movesToIndex.get(search);
+                    if (plays[ind] == '_') {
+                        plays[ind] = xMove ? 'X' : 'O';
+                        break;
+                    } else {
+                        System.out.println("This cell is occupied! Choose another one!");
+                    }
+                } else {
+                    System.out.println("Coordinates should be from 1 to 3!");
+                }
+            } catch (Exception e) {
+                System.out.println("You should enter numbers!");
+            }
+        }
+        printBoard();
+    }
+    private int checkOk(char x, char y, char z) {
+        if (x != '_' && x == y && x == z) {
+            return x == 'X' ? 1 : -1;
+        }
+        return 0;
+    }
+    private char checkWin() {
+        int x = 0;
+        int o = 0;
+        for (int[] combo : winCombos) {
+            int res = checkOk(plays[combo[0]],
+                    plays[combo[1]],
+                    plays[combo[2]]);
+            if (res == 1) {
+                ++x;
+            } else if (res == -1) {
+                ++o;
+            }
+        }
+        if (o != 0 && x != 0) {
+            return 'I';
+        } else if (o != 0) {
+            return 'O';
+        } else if (x != 0) {
+            return 'X';
+        } else {
+            String gameState = new String(plays);
+            if (gameState.contains("_")) {
+                return x_ == o_ || x_ == o_ + 1 ? 'N' : 'I';
+            }
+            return x_ == o_ || x_ == o_ + 1 ? 'D' : 'I';
+        }
+
+    }
+    public String getGameCheck() {
+        switch (checkWin()) {
+            case 'X' :
+                return "X wins";
+            case 'O' :
+                return "O wins";
+            case 'I':
+                return "Impossible";
+            case 'D':
+                return "Draw";
+            case 'N':
+                return "Game not finished";
+            default:
+                return "";
+        }
+    }
+    public void printBoard() {
+        System.out.println("---------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(plays[i * 3 + j] + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("---------");
+    }
+}
+public class TicTacToeSimple4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // write your code here
+        TicTackSimple4 ticTacToe = new TicTackSimple4(sc);
+        ticTacToe.printBoard();
+        ticTacToe.move();
+    }
+}
